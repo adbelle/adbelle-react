bf6a9af795bedee80ea3515741e0568d18c5b897
Anthony Bellew <abellew@integrate.com>
2019-05-21 14:46:26 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,47 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-body">
+        <div className="App-flex-row">
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+          <div>
+            Holy smokes, a genuine React app.
+          </div>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={logo} className="App-logo" alt="logo" />
+          </a>
+        </div>
+        <div className="App-flex-row">
+          <div>
+            <p>
+              I decided I'd make a small proof of concept React application to prove
+              that I do actually know a thing or two about working in React. What you
+              see before you is that application. It's running in Node with Express
+              serving the content.
+            </p>
+            <p>
+              There's a lot more that you can do with React -- passing state and props
+              between components, nesting, using React-Router and Redux with
+              Sagas, and much more.
+            </p>
+            <p>
+              Think of all the fun we could be having!
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
